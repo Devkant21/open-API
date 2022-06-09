2e229276f404ed2dc0ca08a07b69062fe6710655
pradumnasaraf <pradumnasaraf@gmail.com>
2022-06-09 13:46:36 +0530

    feat: Add mongoDB support

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config(); 
 const app = express();
 
+app.use(express.static('public'))
 app.use(require('./middleware/logger.js'))
+app.use(express.json())
 app.use(require('./routes/router.js'))
 app.use(require('./middleware/404.js'))
 
-PORT = process.PORT || 3001
+
+mongoose.connect(process.env.MONGODB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.on('open', () => {
+    console.log("Connected to MongoDB");
+});
+PORT = process.PORT || 9001
 app.listen(PORT, () => console.log(`Server is live on: http://localhost:${PORT}`))
